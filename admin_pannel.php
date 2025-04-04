5bcabc1cefde80028e2aed9b19d6178ce143827b
atahancelenk <celenksinasiatahan@gmail.com>
2025-04-04 11:33:23 +0300

    Added dashboard section to admin panel

--- admin_pannel.php
+++ admin_pannel.php
@@ -24,5 +24,33 @@
 </head>
 <body>
     <?php include 'admin_header.php'; ?>
+    <div class="line4"></div>
+    <section class="dashboard">
+        <div class="box-container">
+            <div class="box">
+                <?php 
+                    $total_pendings = 0;
+                    $select_pendings = mysqli_query($conn, "SELECT * FROM `orders` WHERE payment_status = 'pending'") or die('query failed');
+                    while ($fetch_pendings = mysqli_fetch_assoc($select_pendings)) {
+                        $total_pendings += $fetch_pendings['total_price'];
+                    }
+                ?>
+                <h3>₹<?php echo $total_pendings; ?></h3>
+                <p>Total Pendings</p>
+            </div>
+            <div class="box">
+                <?php 
+                    $total_completes = 0;
+                    $select_pendings = mysqli_query($conn, "SELECT * FROM `orders` WHERE payment_status = 'complete'") or die('query failed');
+                    while ($fetch_completes = mysqli_fetch_assoc($select_completes)) {
+                        $total_completes += $fetch_completes['total_price'];
+                    }
+                ?>
+                <h3>₹<?php echo $total_completes; ?></h3>
+                <p>Total Completes</p>
+            </div>
+        </div>
+    </section>
+    <script type="text/javascript" src="scripts/script.js"></script>
 </body>
 </html>
