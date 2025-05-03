621eca4e66034becaa2664d4e20bd948673034df
atahancelenk <celenksinasiatahan@gmail.com>
2025-05-03 18:30:07 +0300

    Refactor login and register scripts for improved readability and consistency

--- register.php
+++ register.php
@@ -1,38 +1,39 @@
 <?php
-    session_start();
-    include 'connection.php';
+session_start();
+include 'connection.php';
 
-    if (isset($_POST['submit-btn'])) {
-        $filter_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-        $name = mysqli_real_escape_string($conn, $filter_name);
+if (isset($_POST['submit-btn'])) {
+    $filter_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $name = mysqli_real_escape_string($conn, $filter_name);
 
-        $filter_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-        $email = mysqli_real_escape_string($conn, $filter_email);
+    $filter_email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $email = mysqli_real_escape_string($conn, $filter_email);
 
-        $filter_password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
-        $password = mysqli_real_escape_string($conn, $filter_password);
+    $filter_password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+    $password = mysqli_real_escape_string($conn, $filter_password);
 
-        $filter_cpassword = filter_var($_POST['cpassword'], FILTER_SANITIZE_STRING);
-        $cpassword = mysqli_real_escape_string($conn, $filter_cpassword);
+    $filter_cpassword = filter_var($_POST['cpassword'], FILTER_SANITIZE_STRING);
+    $cpassword = mysqli_real_escape_string($conn, $filter_cpassword);
 
-        $select_user = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email'") or die('Query failed: ' . mysqli_error($conn));
-    
-        if (mysqli_num_rows($select_user) > 0) {
-            $message[] = 'User already exists!';
+    $select_user = mysqli_query($conn, "SELECT * FROM users WHERE email = '$email'") or die('Query failed: ' . mysqli_error($conn));
+
+    if (mysqli_num_rows($select_user) > 0) {
+        $message[] = 'User already exists!';
+    } else {
+        if ($password != $cpassword) {
+            $message[] = 'Confirm password not matched!';
         } else {
-            if ($password != $cpassword) {
-                $message[] = 'Confirm password not matched!';
-            } else {
-                mysqli_query($conn, "INSERT INTO users(name, email, password) VALUES('$name', '$email', '$password')") or die('Query failed: ' . mysqli_error($conn));
-                $message[] = 'Registered successfully!';
-                header('location:login.php');
-            }
+            mysqli_query($conn, "INSERT INTO users(name, email, password) VALUES('$name', '$email', '$password')") or die('Query failed: ' . mysqli_error($conn));
+            $message[] = 'Registered successfully!';
+            header('location:login.php');
         }
     }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -40,21 +41,22 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Register Page</title>
 </head>
+
 <body>
 
     <section class="form-container">
 
         <?php
-            if (isset($message)) {
-                foreach ($message as $msg) {
-                    echo '
+        if (isset($message)) {
+            foreach ($message as $msg) {
+                echo '
                         <div class="message">
                             <span>' . $msg . '</span>
                             <i class="bx bx-x" onclick="this.parentElement.remove();"></i>
                         </div>
-                    ';  
-                }
+                    ';
             }
+        }
         ?>
 
         <form method="post">
@@ -69,4 +71,5 @@
         </form>
     </section>
 </body>
+
 </html>
