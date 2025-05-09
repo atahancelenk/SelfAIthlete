76ccc13e94c765d274c6ec31fbfd52667d8147e8
atahancelenk <celenksinasiatahan@gmail.com>
2025-05-09 18:47:09 +0300

    Update stylesheet link in login and register pages to use signin.css

--- login.php
+++ login.php
@@ -40,7 +40,7 @@ if (isset($_POST['submit-btn'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/signin.css">
     <title>Register Page</title>
 </head>
 
