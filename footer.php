d72e8ce43d6bc363af470c4852ed3622aee40ef6
atahancelenk <celenksinasiatahan@gmail.com>
2025-05-14 14:52:47 +0300

    Add footer.php with layout and contact information

---
+++ footer.php
@@ -0,0 +1,37 @@
+<?php
+// footer.php
+?>
+<footer class="py-4" style="background-color: #34495e; color: white;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>About SelfAIthlete</h5>
+                <p>Your personal fitness companion powered by AI to help you achieve your health goals.</p>
+            </div>
+            <div class="col-md-4">
+                <h5>Quick Links</h5>
+                <ul class="list-unstyled">
+                    <li><a href="home.php" class="text-white" style="text-decoration: none;">Home</a></li>
+                    <li><a href="exercises.php" class="text-white" style="text-decoration: none;">Exercises</a></li>
+                    <li><a href="diet.php" class="text-white" style="text-decoration: none;">Nutrition</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5>Contact</h5>
+                <ul class="list-unstyled">
+                    <li><i class="bi bi-envelope"></i> info@selfaithlete.com</li>
+                    <li><i class="bi bi-telephone"></i> +90 555 123 4567</li>
+                </ul>
+            </div>
+        </div>
+        <hr class="my-4" style="background-color: rgba(255,255,255,0.5);">
+        <div class="text-center">
+            <p>&copy; <?php echo date('Y'); ?> SelfAIthlete. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
