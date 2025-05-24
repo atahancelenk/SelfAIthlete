ebc505773e48754d6112fa0b13783511a24add4b
atahancelenk <celenksinasiatahan@gmail.com>
2025-05-24 11:02:03 +0300

    Update database connection parameters in connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,11 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'selfaithlete_db') or die('Connection failed: ' . mysqli_connect_error());
-?>
+$servername = "localhost";
+$username = "selfaith_atahan";
+$password = "0804d[YBPzi.eB";
+$dbname = "selfaith_selfaithlete_db";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
